c17a29b9c6983aae62f91487d33ea2c9743d32ee
Sarah Bunker <ChemEnCoder@gmail.com>
2023-09-11 09:15:47 -0600

    maping over notes in the return instead of using a different function

--- part2/example1/src/App.jsx
+++ part2/example1/src/App.jsx
@@ -1,26 +1,13 @@
 import Note from "./components/Note"
 
 const App = ({notes}) => {
-  function notesDisplay() {
-    return (
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    )
-  }
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map( note => <Note key={note.id} note={note}/>)}
       </ul>
-      <h2>Another list</h2>
-      {notesDisplay()}
     </div>
   )
 }

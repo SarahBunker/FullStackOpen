7022b9f8505c56523f08634a0b713bcc9654324b
Sarah Bunker <ChemEnCoder@gmail.com>
2023-09-11 10:01:54 -0600

    setting up a form to add new notes, adding an onChange event handler to track when input changes.

--- part2/example1/src/App.jsx
+++ part2/example1/src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
 import Note from "./components/Note"
 
-const App = ({notes}) => {
+const App = ({notesInitial}) => {
+  const [notes, setNotes] = useState(notesInitial);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  function addNote(event) {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  }
+
+  function handleNoteChange (event) {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
 
   return (
     <div>
@@ -8,6 +21,10 @@ const App = ({notes}) => {
       <ul>
         {notes.map( note => <Note key={note.id} note={note}/>)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
